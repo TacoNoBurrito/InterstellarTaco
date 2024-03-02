e0d84bd57491a7c2e7676c31c3cb765cfaeaa7b0
TacoNoBurrito <74216683+TacoNoBurrito@users.noreply.github.com>
2024-03-02 22:45:33 +0000

    asd

--- static/m/config.js
+++ static/m/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/a/',
-    bare: 'https://gointerstellar.app/v/',
+    bare: '/v/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/m/handler.js',
